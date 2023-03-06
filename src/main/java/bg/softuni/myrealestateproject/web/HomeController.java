b2fb859b452631ebbf313ec87b950e174abb0136
Milena <milenka.dimova@gmail.com>
2023-03-06 16:46:59 +0200

    Changes on HomeController

--- src/main/java/bg/softuni/myrealestateproject/web/HomeController.java
+++ src/main/java/bg/softuni/myrealestateproject/web/HomeController.java
@@ -1,14 +1,39 @@
 package bg.softuni.myrealestateproject.web;
 
+import bg.softuni.myrealestateproject.service.OfferService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+    private final OfferService offerService;
 
+    public HomeController(OfferService offerService) {
+        this.offerService = offerService;
+    }
 
     @GetMapping("index")
-    public String index() {
-        return "index";
+    public ModelAndView home(ModelAndView modelAndView) {
+        modelAndView.addObject("properties", this.offerService.findLatestOffers());
+        modelAndView.setViewName("index");
+
+        return modelAndView;
     }
+
+    @GetMapping("/details/")
+    public ModelAndView details(@RequestParam("id") Long id, ModelAndView modelAndView) {
+
+        modelAndView.addObject("property", this.offerService.findById(id));
+        modelAndView.setViewName("property-detail");
+
+        return modelAndView;
+    }
+
+//    @PostMapping("/search")
+//    public ModelAndView search(ModelAndView modelAndView) {
+//
+//    }
 }
