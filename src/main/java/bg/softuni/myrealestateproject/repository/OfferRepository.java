dd9f138cc8db37b11a752fae50fba17e24b34c0c
Milena <milenka.dimova@gmail.com>
2023-03-24 19:59:15 +0200

    Return Page OfferEntity instead List

--- src/main/java/bg/softuni/myrealestateproject/repository/OfferRepository.java
+++ src/main/java/bg/softuni/myrealestateproject/repository/OfferRepository.java
@@ -2,11 +2,9 @@ package bg.softuni.myrealestateproject.repository;
 
 import bg.softuni.myrealestateproject.model.entity.OfferEntity;
 import bg.softuni.myrealestateproject.model.entity.OfferTypeEntity;
-import bg.softuni.myrealestateproject.model.enums.OfferTypeEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
