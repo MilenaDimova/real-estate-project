5f8579e6fdf752aa21db71db7e49b962a5f7d614
Milena <milenka.dimova@gmail.com>
2023-03-31 01:30:07 +0300

    Added maintenance interceptor

--- src/main/java/bg/softuni/myrealestateproject/config/AppBeanConfig.java
+++ src/main/java/bg/softuni/myrealestateproject/config/AppBeanConfig.java
@@ -1,16 +1,28 @@
 package bg.softuni.myrealestateproject.config;
 
+import bg.softuni.myrealestateproject.service.MaintenanceInterceptor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppBeanConfig {
+public class AppBeanConfig implements WebMvcConfigurer {
+
+    private final MaintenanceInterceptor maintenanceInterceptor;
+
+    public AppBeanConfig(MaintenanceInterceptor maintenanceInterceptor) {
+        this.maintenanceInterceptor = maintenanceInterceptor;
+    }
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(maintenanceInterceptor);
+    }
 }
---
+++ src/main/java/bg/softuni/myrealestateproject/web/MaintenanceController.java
@@ -0,0 +1,13 @@
+package bg.softuni.myrealestateproject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MaintenanceController {
+
+    @GetMapping("/maintenance")
+    public String maintenance() {
+        return  "maintenance";
+    }
+}
